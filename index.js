dfccfde62d3dfa082c20deeb8fc1f6cb4e3960c1
Marcus Ricketts <marcusricketts@Marcuss-MacBook-Pro.local>
2021-02-02 21:03:14 -0500

    I created the seperate files that will contain the data needed to run my RPSLS project

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// validate user input
+// incorporate inheritance into game
+// store all gesture/choices in an array
+// utilize gestures in the array
+// allow the correct player to win a given round based on the choices made by each player
+// make the game winner be the player with the best score of three 
+// make option for (human vs AI) and (human vs human)
+
+class Game {
+    
+}
